b81fcf2ee4e7882480dc4916f63b4a6b70cec8f3
Spectre <ma.hamzaoui.09@gmail.com>
2024-11-18 19:49:24 +0100

    video.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,6 +8,21 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+	
+
+	float nbr_of_bytes = w*h*3;
+	if (strcmp(unit,"bt")== 0){
+		return nbr_of_bytes * 8 ;
+	}else if (strcmp(unit,"ko")== 0){
+		return nbr_of_bytes *1/1024;
+	}else if (strcmp(unit , "mo")== 0){
+		return nbr_of_bytes * 1/(1024*1024);
+	}else if (strcmp(unit , "go")== 0){
+		return nbr_of_bytes * 1/(1024*1024*1024);
+	}else
+		return nbr_of_bytes;
+
+
 
    // YOUR CODE HERE - END
    return 0;
--- video.c
+++ video.c
@@ -10,7 +10,14 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+	float bytes_of_colored_section = w*h*3*durationMovie*fps;
+	float bytes_of_credit_section = w*h*durationCredits*fps;
+	float total_bytes = bytes_of_colored_section + bytes_of_credit_section;  
+	if (strcmp(unit,"bt")== 0)return total_bytes*8 ;
+	if (strcmp(unit,"ko")== 0)return total_bytes* 1/1024 ;
+	if (strcmp(unit,"mo")== 0)return total_bytes*1 / (1024*1024) ;
+	if (strcmp(unit,"go")== 0)return total_bytes* 1/(1024*1024*1024) ;
+	
    // YOUR CODE HERE - END
    return 0;
 }
