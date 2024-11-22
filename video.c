4cdafa12e37e239236c752acaee105cb7ec46e30
Hamzaoui-mohamed-anis <ma.hamzaoui.09@gmail.com>
2024-11-22 18:23:24 +0100

    Update video.c

--- video.c
+++ video.c
@@ -11,12 +11,12 @@
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
 	float bytes_of_colored_section = w*h*3*durationMovie*fps;
-	float bytes_of_credit_section = w*h*durationCredits*fps;
+	float bytes_of_credit_section = w*h/8*durationCredits*fps;
 	float total_bytes = bytes_of_colored_section + bytes_of_credit_section;  
 	if (strcmp(unit,"bt")== 0)return total_bytes*8 ;
-	if (strcmp(unit,"ko")== 0)return total_bytes* 1/1024 ;
-	if (strcmp(unit,"mo")== 0)return total_bytes*1 / (1024*1024) ;
-	if (strcmp(unit,"go")== 0)return total_bytes* 1/(1024*1024*1024) ;
+	if (strcmp(unit,"ko")== 0)return total_bytes/1024 ;
+	if (strcmp(unit,"mo")== 0)return total_bytes/(1024*1024) ;
+	if (strcmp(unit,"go")== 0)return total_bytes/(1024*1024*1024) ;
 	
    // YOUR CODE HERE - END
    return 0;
